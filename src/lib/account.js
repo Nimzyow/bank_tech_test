eb26837e3b8fba04e9c46e985088d67809a7d121
nimzyow <n_soufiani@hotmail.com>
2020-04-20 22:27:55 +0100

    testing done for statement function of Account

--- src/lib/account.js
+++ src/lib/account.js
@@ -1,14 +1,21 @@
-let Account = function (log, transaction) {
+let Account = function (log, transaction, statement) {
   this._log = log || new Log();
   this._transaction = transaction || new Transaction();
+  this._statement = statement || new Statement();
 };
 
-Account.prototype.withdraw = function () {
-  this._transaction.withdrawal();
+Account.prototype.withdraw = function (amount, date) {
+  this._log.addToHistory(this._transaction.withdrawal(amount, date));
 };
 
-Account.prototype.deposit = function () {};
+Account.prototype.deposit = function (amount, date) {
+  this._log.addToHistory(this._transaction.deposit(amount, date));
+};
 
 Account.prototype.log = function () {
   return this._log;
+};
+
+Account.prototype.statement = function () {
+  this._statement.print();
 };
--- src/lib/account.spec.js
+++ src/lib/account.spec.js
@@ -1,17 +1,16 @@
 describe("Account.js", () => {
-  let log = jasmine.createSpy("log", ["history", "addToHistory"]);
+  //create spy here for rest of tests to use
+  let log = jasmine.createSpyObj("log", ["history", "addToHistory"]);
   let transaction = jasmine.createSpyObj("transaction", [
     "withdrawal",
     "deposit",
   ]);
-  transaction.withdrawal.and.callFake(function () {});
-  // transaction.withdrawal = function () {
-  //   return "something";
-  // };
+  let statement = jasmine.createSpyObj("statement", ["print"]);
+
   describe("function not to be undefined for", () => {
     let account;
     beforeEach(() => {
-      account = new Account(log, transaction);
+      account = new Account(log, transaction, statement);
     });
     it("withdraw", () => {
       expect(account.withdraw).not.toBeUndefined();
@@ -22,18 +21,62 @@ describe("Account.js", () => {
     it("log", () => {
       expect(account.log).not.toBeUndefined();
     });
+    it("statement", () => {
+      expect(account.statement).not.toBeUndefined();
+    });
   });
+
   describe("initializes", () => {
     it("with log class", () => {
-      account = new Account(log, transaction);
+      account = new Account(log, transaction, statement);
       expect(account.log()).toEqual(log);
     });
   });
-  describe("function", () => {
+
+  describe("withdraw function", () => {
     it("withdraw delegates to Transaction.withdrawal", () => {
-      account = new Account(log, transaction);
+      account = new Account(log, transaction, statement);
+      transaction.withdrawal.and.callFake(function () {
+        return {};
+      });
       account.withdraw();
-      expect(account._transaction.withdrawal).toHaveBeenCalledTimes(1);
+      expect(account._transaction.withdrawal).toHaveBeenCalled();
+    });
+    it("withdraw delegates to Log.addToHistory", () => {
+      account = new Account(log, transaction, statement);
+      log.addToHistory.and.callFake(function () {
+        return {};
+      });
+      account.withdraw();
+      expect(account._log.addToHistory).toHaveBeenCalled();
+    });
+  });
+
+  describe("deposit function", () => {
+    it("deposit delegates to Transaction.deposit", () => {
+      account = new Account(log, transaction, statement);
+      transaction.deposit.and.callFake(function () {
+        return {};
+      });
+      account.deposit();
+      expect(account._transaction.deposit).toHaveBeenCalled();
+    });
+    it("deposit delegates to Log.addToHistory", () => {
+      account = new Account(log, transaction, statement);
+      log.addToHistory.and.callFake(function () {
+        return {};
+      });
+      account.deposit();
+      expect(account._log.addToHistory).toHaveBeenCalled();
+    });
+  });
+
+  describe("statement function", () => {
+    it("statement delegates to Statement.print", () => {
+      account = new Account(log, transaction, statement);
+      statement.print.and.callFake(function () {});
+      account.statement();
+      expect(account._statement.print).toHaveBeenCalled();
     });
   });
 });
