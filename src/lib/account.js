f5e52a306a98cab7c0923f9672a99300844085fc
nimzyow <n_soufiani@hotmail.com>
2020-04-20 16:33:06 +0100

    created spy and tested for initializtion

--- src/lib/account.js
+++ src/lib/account.js
@@ -1,3 +1,11 @@
-let Account = function () {};
+let Account = function (log) {
+  this._log = log || new Log();
+};
 
 Account.prototype.withdraw = function () {};
+
+Account.prototype.deposit = function () {};
+
+Account.prototype.log = function () {
+  return this._log;
+};
--- src/lib/account.spec.js
+++ src/lib/account.spec.js
@@ -1,8 +1,25 @@
 describe("Account.js", () => {
   describe("function not to be undefined for", () => {
+    let account;
+    let log = jasmine.createSpy("log", ["history"]);
+    beforeEach(() => {
+      account = new Account(log);
+    });
     it("withdraw", () => {
-      const account = new Account();
       expect(account.withdraw).not.toBeUndefined();
+    });
+    it("deposit", () => {
+      expect(account.deposit).not.toBeUndefined();
+    });
+    it("log", () => {
+      expect(account.log).not.toBeUndefined();
+    });
+  });
+  describe("initializes", () => {
+    it("with log class", () => {
+      let log = jasmine.createSpy("log", ["history"]);
+      account = new Account(log);
+      expect(account.log()).toEqual(log);
     });
   });
 });
