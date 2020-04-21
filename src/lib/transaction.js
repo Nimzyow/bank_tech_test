ba0212d4c28b79d576429c5f3a595558c52d3036
nimzyow <n_soufiani@hotmail.com>
2020-04-21 22:46:50 +0100

    refactored transaction.js

--- src/lib/transaction.js
+++ src/lib/transaction.js
@@ -2,7 +2,7 @@ let Transaction = function () {};
 
 Transaction.prototype.withdrawal = function (amount, date, history) {
   this.firstDepositCheck(history);
-  history = history.sort((a, b) => b.date - a.date);
+  history = this.sortHistory(history);
   let calcBalance = history[0].balance - amount;
   this.enoughMoneyInAccountCheck(calcBalance);
   return { debit: amount, date: date, balance: calcBalance };
@@ -12,7 +12,7 @@ Transaction.prototype.deposit = function (amount, date, history) {
   if (history.length === 0) {
     return { credit: amount, date: date, balance: amount };
   } else {
-    history = history.sort((a, b) => b.date - a.date);
+    history = this.sortHistory(history);
     let calcBalance = history[0].balance + amount;
     return { credit: amount, date: date, balance: calcBalance };
   }
@@ -28,4 +28,8 @@ Transaction.prototype.enoughMoneyInAccountCheck = function (calcBalance) {
   if (calcBalance < 0) {
     throw new Error("not enough money in your account to withdraw that amount");
   }
+};
+
+Transaction.prototype.sortHistory = function (history) {
+  return history.sort((a, b) => b.date - a.date);
 };
